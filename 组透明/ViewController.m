9b5854f34e4be2c8557dbf5f0fc75a46482cc63c
codeliu6572 <codeliu6572@163.com>
2016-03-11 16:19:45 +0800

    AlphaAndOpacity
    AlphaAndOpacity

--- 组透明/ViewController.m
+++ 组透明/ViewController.m
@@ -13,10 +13,43 @@
 @end
 
 @implementation ViewController
-
+- (UIButton *)customButton
+{
+    //create button
+    CGRect frame = CGRectMake(0, 0, 150, 50);
+    UIButton *button = [[UIButton alloc] initWithFrame:frame];
+    button.backgroundColor = [UIColor whiteColor];
+    button.layer.cornerRadius = 10;
+    //add label
+    frame = CGRectMake(20, 10, 110, 30);
+    UILabel *label = [[UILabel alloc] initWithFrame:frame];
+    label.text = @"Hello World";
+    label.backgroundColor=[UIColor whiteColor];
+    label.textAlignment = NSTextAlignmentCenter;
+    [button addSubview:label];
+    return button;
+}
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
+    UIView *view=[[UIView alloc]initWithFrame:CGRectMake(0, 0, 320, 568)];
+    view.backgroundColor=[UIColor blackColor];
+    view.layer.opacity=0.5;
+    [self.view addSubview:view];
+    
     // Do any additional setup after loading the view, typically from a nib.
+    self.view.backgroundColor=[UIColor colorWithRed:0.75f green:0.75f blue:0.75f alpha:1.00f];
+    UIButton *button1 = [self customButton];
+    button1.center = CGPointMake(160, 150);
+    [view addSubview:button1];
+    //create translucent button
+    UIButton *button2 = [self customButton];
+    button2.center = CGPointMake(160, 250);
+    button2.alpha = 0.5;
+    [view addSubview:button2];
+    //enable rasterization for the translucent button
+//    button2.layer.shouldRasterize = YES;
+//    button2.layer.rasterizationScale = [UIScreen mainScreen].scale;
 }
 
 - (void)didReceiveMemoryWarning {
